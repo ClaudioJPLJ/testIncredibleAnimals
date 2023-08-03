795a5d614be4e7d69046269a13d756fd5766960d
Cláudio José <claudiojpdlj1808@gmail.com>
2023-08-02 21:11:16 -0300

    menu-mobile refactored

--- scrip/menu-mobile.js
+++ scrip/menu-mobile.js
@@ -1,15 +1,21 @@
-export default function menuMobile() {
-  const receiveMenuClick = document.querySelector('[data-menuMobile]');
-  const receiveMenu = document.querySelector('[data-menumobile] + ul');
-  const menuClicked = () => {
-    [receiveMenu, receiveMenuClick].forEach((element) => {
+export default class MenuMobile {
+  constructor(menuButton, menuElements) {
+    this.receiveMenuClick = document.querySelector(menuButton);
+    this.receiveMenu = document.querySelector(menuElements);
+    this.menuClicked = this.menuClicked.bind(this);
+  }
+
+  menuClicked() {
+    [this.receiveMenu, this.receiveMenuClick].forEach((element) => {
       element.classList.toggle('active');
     });
-  };
+  }
 
-  if (receiveMenuClick) {
-    ['click', 'touchstart'].forEach(eventListener => {
-      receiveMenuClick.addEventListener(eventListener, menuClicked);
-    });
+  init() {
+    if (this.receiveMenuClick && this.receiveMenu) {
+      ['click', 'touchstart'].forEach(eventListener => {
+        this.receiveMenuClick.addEventListener(eventListener, this.menuClicked);
+      });
+    }
   }
 }
